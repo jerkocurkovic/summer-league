f2992f7fdaeed788b86df6675c3ab7d48b0307e4
jerkocurkovic <jerko.curkovic1@gmail.com>
2024-03-29 22:05:29 +0100

    Updated ljetna liga

---
+++ code/app/competition/sponsors/page.tsx
@@ -0,0 +1,19 @@
+
+export const metadata = {
+    title: "Sponzori",
+  };
+  
+function CompetitionSponsors(){
+    return(
+        <div className="flex flex-col max-w-[850px] min-w-[300px] xl:max-w-[850px] mx-auto bg-white overflow-hidden mb-10">
+
+            <h1 className="text-3xl font-abeezee font-bold text-brand-black-500 mb-4">
+            Sponzori
+            </h1>
+        </div>
+                
+
+    );
+}
+
+export default CompetitionSponsors;
